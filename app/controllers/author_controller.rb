93ad4f5d27f6f4ad4ba6966be4bfa04f53519ea6
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-10-17 12:35:01 +0100

    Optimized version

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -2,6 +2,5 @@ class AuthorController < ApplicationController
 
   def index
     @authors = Author.includes(:articles).all.paginate(:page => params[:page], :per_page => 30)
-
-  end
+  end 
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,28 +1,14 @@
 class Author < ActiveRecord::Base
   has_many :articles
 
-  def self.generate_authors(count=1000)
-    count.times do
-      Fabricate(:author)
-    end
-    
-  end
-
-   scope :with_most_upvoted_article, -> { includes(:articles).order('articles.upvotes asc').last.name }  
+  scope :with_most_upvoted_article, -> { includes(:articles).order('articles.upvotes asc').last.name }  
 
   scope :most_prolific_writer, -> { order('articles_count asc').last }
 
-  # def self.most_prolific_writer
-  #   all.sort_by{|a| a.articles.count }.last
-  # end
-
-  # def self.with_most_upvoted_article
-  #   all.sort_by do |auth|
-  #     auth.articles.sort_by do |art|
-  #       art.upvotes
-  #     end.last
-  #   end.last.name
-  # end
-
-
+  def self.generate_authors(count=1000)
+    count.times do
+      Fabricate(:author)
+    end 
+  end
+  
 end
